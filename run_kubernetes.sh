fbab77004dce661acd8992ccaa628667af5f44fe
sambonuruddeen <sambonuruddeen@gmail.com>
2022-08-15 14:00:51 +0100

    Task 6: Deploy with Kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=sambonuruddeen/udacityproject
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacityproject --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udacityproject 8000:80
+
 
